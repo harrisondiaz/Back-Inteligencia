c2c163458f6068d48d417e199ca45a111b408fb2
Harrison Diaz <harryjhoi01@gmail.com>
2023-05-30 17:03:59 -0500

    Holi

--- app.py
+++ app.py
@@ -39,4 +39,5 @@ def predict_route():
     return jsonify({"clase_de_hoja": tipo_hoja[0]})
 
 if __name__ == "__main__":
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True, host='0.0.0.0', port=os.getenv("PORT", default=5000))
+
